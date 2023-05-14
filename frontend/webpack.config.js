428d89c77b3b33fc2bd05772a9c71a14ced744b6
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-05-15 00:34:13 +0300

    infra: init frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: [
+      path.resolve(__dirname, 'src', 'index.tsx'),
+      path.resolve(__dirname, 'src', 'index.scss'),
+    ],
+  },
+  context: path.join(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/, loader: 'css-loader' },
+      { test: /\.svg$/, loader: 'file-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    fallback: {
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
