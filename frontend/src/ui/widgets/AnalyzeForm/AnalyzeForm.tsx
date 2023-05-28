b00b361b2861c980e7b296b9c203b659ef1bc87a
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-05-28 22:32:33 +0300

    fix unom iterator

--- frontend/src/ui/widgets/AnalyzeForm/AnalyzeForm.tsx
+++ frontend/src/ui/widgets/AnalyzeForm/AnalyzeForm.tsx
@@ -84,7 +84,7 @@ const AnalyzeForm = ({ children }: Props) => {
           />
           <Text>Выберите адрес</Text>
           <MultiSelect
-            value={analyzeFormData.unom.map(a => a.toString())}
+            value={analyzeFormData.unom?.map(a => a.toString())}
             data={convertToAddressesItems(options?.addresses || [])}
             limit={20}
             searchable
