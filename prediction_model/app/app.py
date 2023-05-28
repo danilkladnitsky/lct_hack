ebbc4627b99a05b9df9401d02874293a856d6a26
kirrog <kirrog@zertan.ru>
2023-05-28 22:50:29 +0300

    feat: one object per route

--- prediction_model/app/app.py
+++ prediction_model/app/app.py
@@ -10,29 +10,29 @@ main_model = MainModel()
 @app.route('/model_state_works', methods=['POST'])
 def model_works():
     content = request.get_json()
-    unom_list = content['unom']
+    unom = content['unom']
     included_source = content['source']
     included_work_type = content['work_type']
     start_date = content['start_date']
     end_date = content['end_date']
     status = True
-    result = main_model.predict_works(unom_list, included_source, included_work_type)
-    result_dict = {k: v for k, v in zip(unom_list, result)}
-    return {"unom": result_dict}
+    result = main_model.predict_works([unom], included_source, included_work_type)
+    # result_dict = {k: v for k, v in zip(unom_list, result)}
+    return {"unom": unom, "work_type": result[0]}
 
 
 @app.route('/model_state_incedents', methods=['POST'])
 def model_incedents():
     content = request.get_json()
-    unom_list = content['unom']
+    unom = content['unom']
     included_source = content['source']
     includet_work_type = content['work_type']
     start_date = content['start_date']
     end_date = content['end_date']
     status = True
-    result = main_model.predict_incedents(unom_list)
-    result_dict = {k: v for k, v in zip(unom_list, result)}
-    return {"result": result_dict}
+    result = main_model.predict_incedents([unom])
+    # result_dict = {k: v for k, v in zip(unom_list, result)}
+    return {"unom": unom, "work_type": result[0]}
 
 
 if __name__ == '__main__':
