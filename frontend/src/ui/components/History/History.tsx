5172443e3a2ea8e7c8e565cd2d9c6cea8e74a400
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-06-10 15:19:20 +0300

    feature: add sorting

--- frontend/src/ui/components/History/History.tsx
+++ frontend/src/ui/components/History/History.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
-import { Loader, Pagination } from '@mantine/core';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Button, Group, Loader, Pagination } from '@mantine/core';
 import useGetHistory from 'api/hooks/use-get-history';
+import { SortAscending, SortDescending } from 'tabler-icons-react';
+import { HistoryRecord as HistoryRecordType } from 'types/history';
 import Title from 'ui/shared/Title/Title';
 
 import useCombinedStore from 'store';
@@ -11,25 +13,49 @@ import styles from './History.module.scss';
 
 const History = () => {
   const { status, mutate: fetchHistory } = useGetHistory();
+  const records = useCombinedStore(state => state.records);
+  const [isDescending, setIsDescending] = useState(true);
 
   useEffect(() => {
     fetchHistory();
   }, []);
 
+  const toggleSort = () => {
+    setIsDescending(v => !v);
+  };
+
+  const filteredRecords = useMemo(() => {
+    return records.sort((a, b) => {
+      if (isDescending) {
+        return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+      }
+
+      return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+    });
+  }, [records, isDescending]);
+
   return (
     <div className={styles.history}>
       <Title order={2}>История запросов</Title>
-      {status === 'loading' ? <Loader /> : <RecordList />}
+      <Group>
+        <Button
+          leftIcon={isDescending ? <SortDescending /> : <SortAscending />}
+          onClick={toggleSort}>
+          Сортировать
+        </Button>
+      </Group>
+      {status === 'loading' ? <Loader /> : <RecordList records={filteredRecords} />}
       <Pagination total={1}
       />
     </div>
   );
 };
 
-const RecordList = () => {
-  const records = useCombinedStore(state => state.records);
+type Props = {
+  records: HistoryRecordType[];
+}
 
-  console.log(records);
+const RecordList = ({ records }: Props) => {
 
   return <div className={styles.recordList}>
     {records
